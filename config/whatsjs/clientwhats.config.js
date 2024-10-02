97be07243785cc0efa817698971d150a929faebe
Carlos Giongo <carlosgiongoo@gmail.com>
2024-10-02 17:14:24 -0300

    fix:changed axios

--- config/whatsjs/clientwhats.config.js
+++ config/whatsjs/clientwhats.config.js
@@ -1,77 +1,93 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const ZeroMq = require('../zeromq/zeromq.config');
-const qrcode = require('qrcode-terminal');
-const axios = require('axios');
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const ZeroMq = require("../zeromq/zeromq.config");
+const qrcode = require("qrcode-terminal");
+const axios = require("axios");
 // const mountResponse = require('../../utils/mountResponse');
 
 /**
- * @param {ZeroMq} zeroMq 
+ * @param {ZeroMq} zeroMq
  */
-function startWhatsJs(zeroMq){
-    console.log('Starting WhatsJs...');
+function startWhatsJs(zeroMq) {
+  console.log("Starting WhatsJs...");
 
-    const client = new Client({
-        puppeteer: {
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-            ],
-        },
-        authStrategy: new LocalAuth({
-            dataPath: 'sessions'
-        }),
-    });
+  const client = new Client({
+    puppeteer: {
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
+    authStrategy: new LocalAuth({
+      dataPath: "sessions",
+    }),
+  });
 
-    client.once('ready', () => {
-        console.log('---------------------------------');
-        console.log('|      Client is ready!         |');
-        console.log('---------------------------------');
-    });
-    
-    client.on('qr', (qr) => {
-        qrcode.generate(qr, {small: true});
-    });
-    
-    client.on('message_create', message => {
-        let final_message = {
-            type: 'message',
-            from: message.from,
-            content: message.body,
-            response: null
-        };
-        zeroMq.sendMessage(JSON.stringify(final_message));
-    });
+  client.once("ready", () => {
+    console.log("---------------------------------");
+    console.log("|      Client is ready!         |");
+    console.log("---------------------------------");
+  });
 
-    client.initialize();
+  client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
 
-    //--------------------- Event listener to actions ---------------------//
+  client.on("message_create", (message) => {
+    let final_message = {
+      type: "message",
+      from: message.from,
+      content: message.body,
+      response: null,
+    };
+    zeroMq.sendMessage(JSON.stringify(final_message));
+  });
 
-    zeroMq.on('message', (originalMessage) => {
-        let message_parsed = JSON.parse(originalMessage);
-    
-        // -- Response to user
-        if (message_parsed.type === 'response' && message_parsed.response) {
-            client.sendMessage(message_parsed.from, message_parsed.response);
-        }
-    
-        // -- Actions
-        if (message_parsed.type === 'message') {
+  client.initialize();
+
+  //--------------------- Event listener to actions ---------------------//
+
+  zeroMq.on("message", (originalMessage) => {
+    let message_parsed = JSON.parse(originalMessage);
+
+    // -- Response to user
+    if (message_parsed.type === "response" && message_parsed.response) {
+      client.sendMessage(message_parsed.from, message_parsed.response);
+    }
+
+    // -- Actions
+    if (message_parsed.type === "message") {
+      try {
+        let actions = require("../webhooks/actions.json");
+        actions.forEach((action) => {
+          if (
+            action.event === message_parsed.type &&
+            (action.content === message_parsed.content ||
+              action.content === "*")
+          ) {
             try {
-                let actions = require('../webhooks/actions.json');
-                actions.forEach(action => {
-                    if (action.event === message_parsed.type && (action.content === message_parsed.content || action.content === '*')) {
-                        try {
-                            axios.post(action.webhook, message_parsed);
-                        } catch (error) {
-                            console.error('Error on action:', action, error); // Log the specific action and error
-                        }
-                    }
+              fetch(action.webhook, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(message_parsed),
+              })
+                .then((response) => {
+                  if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                  }
+                  return response.json();
+                })
+                .catch((error) => {
+                  console.error("Error on action:", action, error);
                 });
             } catch (error) {
-                console.error("Error processing actions:", error); // Log the error related to actions
+              console.error("Error on action:", action, error); // Log the specific action and error
             }
-        }
-    });
+          }
+        });
+      } catch (error) {
+        console.error("Error processing actions:", error); // Log the error related to actions
+      }
+    }
+  });
 }
 
 module.exports = startWhatsJs;
